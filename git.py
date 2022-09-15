ba24d7c9b9d590affd3f0a669f95fc725b49c84e
arivarasan12 <arivarasan425@gmail.com>
2022-09-15 11:41:50 +0530

    new

--- git.py
+++ git.py
@@ -1,12 +1,17 @@
 import subprocess
 from urllib import response
 import requests
+import time
 def gitfund(url):
+    print(url)
     response = requests. get(url)
+    print("response\n",url)
     file = open("image.png", "wb")
     file. write(response. content)
     file. close()
-gitfund("https://firebasestorage.googleapis.com/v0/b/facedetection-91462.appspot.com/o/Photos%2FJPEG_20220915_084418.jpg?alt=media&token=674030f2-6d04-438a-9069-51980e0d8c06")
-subprocess.run("git add .")
-subprocess.run("git commit -m  'commited'")
-subprocess.run("git push")
+    subprocess.run("git add .")
+    subprocess.run("git commit -m  'commited'")
+    subprocess.run("git push")
+    time.sleep(5)
+    return "executed"
+#gitfund("https://firebasestorage.googleapis.com/v0/b/facedetection-91462.appspot.com/o/Photos%2FJPEG_20220915_084418.jpg?alt=media&token=674030f2-6d04-438a-9069-51980e0d8c06")
